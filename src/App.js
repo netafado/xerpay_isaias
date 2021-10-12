ae8ffc45e32a19ee88962939ae2ed5dde2a8a862
Isaias F. dos Santos <isaias.fran@gmail.com>
2021-10-12 12:44:25 -0300

    project init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App xerpayTest">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
