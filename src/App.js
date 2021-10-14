e9d20952b1cff948982dd47eec56dddb67c5a562
Isaias F. dos Santos <isaias.fran@gmail.com>
2021-10-14 07:44:46 -0300

    Page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import SearchPage from "./pages/search/search";
 function App() {
   return (
     <div className="App xerpayTest">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <SearchPage />
+      </header>
     </div>
   );
 }
---
+++ src/pages/search/search.tsx
@@ -0,0 +1,43 @@
+
+import { useEffect, useRef, useState } from "react"
+import  "./search.scss"
+import catImage from "../../assets/img/cat.svg"
+const SearchPage = () => {
+    const [showInput, setShowInput] = useState(false)
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const chagenState = ()=>{
+        setShowInput(true)
+
+    }
+    useEffect(()=>{
+        if (null !== inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [showInput])
+    return (
+        <section id="search">
+            <div className="container clearfix">
+                <div className="row">
+                    <label htmlFor="search" className="hideLabel">Procurar</label>
+                    <img src={catImage} className="cat-image" alt="Gato roxo" />
+                    {!showInput ?
+                    <div className="card ">
+                        <h1 className="text-center title">Encontre o git <span className="text_brand_one">estrelado</span></h1>
+                        <button onClick={chagenState} className="btn btn_outline_brand_one btn_wrraper">Bora lá</button>
+                    </div>
+                    :
+                    <div className="card ">
+                        <h1 className="text-center title">Nome do usuário ou apelido</h1>
+                        <div className="row btn_wrraper">
+                            <input ref={inputRef}  className="form-input" type="text" name="search" placeholder={"Nome do usuário"} /> 
+                        </div>
+                    </div>
+                    }                    
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SearchPage
